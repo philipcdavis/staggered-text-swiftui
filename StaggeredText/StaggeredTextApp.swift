186d9cfcbbd56309900b2afc3654d81d108cb85a
Philip Davis <philipcdavis11@gmail.com>
2021-01-12 12:43:25 -0500

    Initial Commit

---
+++ StaggeredText/StaggeredTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredTextApp.swift
+//  StaggeredText
+//
+//  Created by Philip Davis on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
